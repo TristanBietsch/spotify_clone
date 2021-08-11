14ab84c814642dd117f9edc688fd74d90354cee4
Tristan Bietsch <tristanmbietsch@gmail.com>
2021-08-11 18:24:11 -0500

    login page updated

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Login.css'
+
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F5%2F56%2FSpotify_logo_horizontal_black.jpg&f=1&nofb=1" alt =""/>
+            {/* login w button */}
+            <a>LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login;
